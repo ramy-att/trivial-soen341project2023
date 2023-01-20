3c204141246e1af1e06d57935fdbde7c882059d0
Ramy Attalla <83047251+ramy-att@users.noreply.github.com>
2023-01-20 11:24:04 -0500

    clean up

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,16 @@ import React from "react";
 import { Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SignIn from "./pages/SignIn";
-import OurNav from "./components/NavBar/OurNav";
+
 const App = () => {
   return (
     <div className="App">
-      <OurNav>
-        <Route path="/welcome">
-          <HomePage />
-        </Route>
-        <Route path="/signin">
-          <SignIn />
-        </Route>
-      </OurNav>
+      <Route path="/welcome">
+        <HomePage />
+      </Route>
+      <Route path="/signin">
+        <SignIn />
+      </Route>
     </div>
   );
 };
