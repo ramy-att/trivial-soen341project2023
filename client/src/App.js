35d362731a90375078700096d596a8c1fa60f42a
Khaled Saleh <93226474+khaled106@users.noreply.github.com>
2023-01-20 16:34:24 -0500

    Changed the Welcome message

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 function App() {
-  return <div className="App">Hello World!</div>;
-  <div className="App">HAYYYY!</div>;
+  return <div className="App">Hello Worldsss!</div>;
+  
 }
 
 export default App;
