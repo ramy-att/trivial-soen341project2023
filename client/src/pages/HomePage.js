40ac256322102ac79fcb1506cd183f0e9327c70b
Ramy Attalla <83047251+ramy-att@users.noreply.github.com>
2023-02-11 20:49:47 -0500

    new "Apply" button &  DataTable funcs

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,92 @@
 import React from "react";
 import DataTable from "../components/DataTable/DataTable";
+import Button from "../components/Buttons/Button";
 const HomePage = () => {
   return (
     <DataTable
-      header={["ID", "Name", "Other", "Other2"]}
-      data={[{ ID: "1", Name: "Ramy", Other: "OtherTxt", Other2: "Other2Txt" }]}
+      header={[
+        "Apply",
+        "Term",
+        "Title",
+        "Organization",
+        "Location",
+        "Other",
+        "Other",
+      ]}
+      data={[
+        {
+          Apply: <Button type="apply">Apply</Button>,
+          Term: "Summer 2023",
+          Title: "Frontend Intern",
+          Organization: "Triviality",
+          Location: "Remote",
+          Other1: "Other",
+          Other2: "Other2",
+        },
+        {
+          Apply: <button>Apply</button>,
+          Term: "Summer 2023",
+          Title: "Frontend Intern",
+          Organization: "Triviality",
+          Location: "Remote",
+          Other1: "Other",
+          Other2: "Other2",
+        },
+        {
+          Apply: <button>Apply</button>,
+          Term: "Summer 2023",
+          Title: "Frontend Intern",
+          Organization: "Triviality",
+          Location: "Remote",
+          Other1: "Other",
+          Other2: "Other2",
+        },
+        {
+          Apply: <button>Apply</button>,
+          Term: "Summer 2023",
+          Title: "Frontend Intern",
+          Organization: "Triviality",
+          Location: "Remote",
+          Other1: "Other",
+          Other2: "Other2",
+        },
+        {
+          Apply: <button>Apply</button>,
+          Term: "Summer 2023",
+          Title: "Frontend Intern",
+          Organization: "Triviality",
+          Location: "Remote",
+          Other1: "Other",
+          Other2: "Other2",
+        },
+        {
+          Apply: <button>Apply</button>,
+          Term: "Summer 2023",
+          Title: "Frontend Intern",
+          Organization: "Triviality",
+          Location: "Remote",
+          Other1: "Other",
+          Other2: "Other2",
+        },
+        {
+          Apply: <button>Apply</button>,
+          Term: "Summer 2023",
+          Title: "Frontend Intern",
+          Organization: "T",
+          Location: "Remote",
+          Other1: "Other",
+          Other2: "Other2",
+        },
+      ]}
+      footer={[
+        "Apply",
+        "Term",
+        "Title",
+        "Organization",
+        "Location",
+        "Other",
+        "Other",
+      ]}
     />
   );
 };
