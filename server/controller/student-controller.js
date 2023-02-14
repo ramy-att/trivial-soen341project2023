5b73192a120400cee3f224139ba94abaf6dfb887
TarekElalfi <elalfi.tarek@hotmail.com>
2023-02-13 19:31:03 -0500

    updated put methd

--- server/controller/student-controller.js
+++ server/controller/student-controller.js
@@ -3,6 +3,7 @@
 const Student = require("../model/student");
 
 //get all the users from the database
+//GET START
 const getAllStudents = async (req, res, next) => {
   //find all the students in the database
   let students;
@@ -13,22 +14,24 @@ const getAllStudents = async (req, res, next) => {
     return next(err);
   }
   if (!students) {
-    return res.status(500).json({ message: "Internal Server Error" }); //checking if we dont have student (false/some error) return the respons to the server as 500
+    return res.status(500).json({ err: "Internal Server Error" }); //checking if we dont have student (false/some error) return the respons to the server as 500
   }
   return res.status(200).json({ students });
 };
+//GET END
+//POST START AKA create a new user
 const addStudent = async (req, res, next) => {
   //to add a new Student without errors AKA post
   const { studentName, studentEmail, studentPassword } = req.body; // we post in the body of the API
   if (
     !studentName &&
-    studentName.trim() == "" &&
+    studentName.trim() === "" &&
     !studentEmail &&
     studentEmail.trim() === "" &&
     !studentPassword &&
     studentPassword.length > 6
   ) {
-    return res.status(422).json({ message: "Invaild data for student" });
+    return res.status(422).json({ err: "Invaild data for student" });
   } // return error message if data is wrong or missing
   let student;
   try {
@@ -43,13 +46,42 @@ const addStudent = async (req, res, next) => {
     return next(err);
   }
   if (!student) {
-    return res
-      .status(500)
-      .json({ message: "Cannot save the user due to error" });
+    return res.status(500).json({ err: "Cannot save the user due to error" });
   }
-  return req.status(201).json({ student }); // 201 is everything goes well return a student Objs
+  return res.status(201).json({ student }); // 201 is everything goes well return a student Objs
+}; // POST ENDS
+//PUT STARTS
+const updateStudent = async (req, res, next) => {
+  const id = req.params.id;
+  const { studentName, studentEmail, studentPassword } = req.body;
+  if (
+    !studentName &&
+    studentName.trim() === "" &&
+    !studentEmail &&
+    studentEmail.trim() === "" &&
+    !studentPassword &&
+    studentPassword.length > 6
+  ) {
+    return res.status(422).json({ err: "Invaild data for student" });
+  }
+  let stu;
+  try {
+    stu = await Student.findByIdAndUpdate(id, {
+      studentName,
+      studentEmail,
+      studentPassword
+    });
+  } catch (err) {
+    return next(err);
+  }
+  if (!stu) {
+    return res.status(500).json({ err: "unable to save the student info" });
+  }
+  return res.status(200).json({message:"updated Successfully"});
 };
+
 exports.getAllStudents = getAllStudents;
 exports.addStudent = addStudent;
+exports.updateStudent = updateStudent;
 //student is the model
 //students is the controller
--- server/routes/student-routes.js
+++ server/routes/student-routes.js
@@ -1,10 +1,11 @@
 //contain all the route structure of the server where the api will be called
 const express = require('express');
-const { getAllStudents, addStudent } = require('../controller/student-controller');
+const { getAllStudents, addStudent, updateStudent } = require('../controller/student-controller');
 
 const studentRouter = express.Router();// contains all the request methods(get,post,put,delete)
 
-studentRouter.get("/",getAllStudents);
-studentRouter.post("/",addStudent);
+studentRouter.get("/",getAllStudents);//get
+studentRouter.post("/",addStudent);//create
+studentRouter.put("/:id",updateStudent);//update
 
 module.exports = studentRouter;
