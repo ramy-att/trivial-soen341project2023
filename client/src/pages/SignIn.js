d1d965c09dc771733d0e4c9fc3f59bbf08b1e674
Ramy Attalla <83047251+ramy-att@users.noreply.github.com>
2023-02-13 22:41:41 -0500

    fixes

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,6 +1,40 @@
 import React from "react";
-
+import DataTable from "../components/DataTable/DataTable.js"
 const SignIn = () => {
-  return <h1> Sign In Page</h1>;
+  return  <DataTable
+  header={["Header0", "Header1", "Header2", "Header3", "Header4"]}
+  footer={["Footer0", "Footer1", "Footer2", "Footer3", "Footer4"]}
+  data={[
+    {
+      Item0: "Item0",
+      Item1: "Item1",
+      Item2: "Item2",
+      Item3: "Item3",
+      Item4: "Item4",
+    },
+    {
+      Item0: "Item0",
+      Item1: "Item1",
+      Item2: "Item2",
+      Item3: "Item3",
+      Item4: "Item4",
+    },
+    {
+      Item0: "Item0",
+      Item1: "Item1",
+      Item2: "Item2",
+      Item3: "Item3",
+      Item4: "Item4",
+    },
+    {
+      Item0: "Item0",
+      Item1: "Item1",
+      Item2: "Item2",
+      Item3: "Item3",
+      Item4: "Item400",
+    },
+  ]}
+  maxRows={2}
+  /> ;
 };
 export default SignIn;
