61b71c84a8846bb4edc12d3a93c482799c4dce4f
TarekElalfi <elalfi.tarek@hotmail.com>
2023-02-21 16:36:34 -0500

    latest edit to the fix *added port*

--- server/index.js
+++ server/index.js
@@ -4,8 +4,7 @@ const express = require("express");
 
 const studentRouter = require("./routes/student-routes");
 
-const PORT = 3001;
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
 app.use(cors());
