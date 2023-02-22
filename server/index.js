51e0958adc96cafb8b2becffa5b07db382a2df3c
Ramy Attalla <83047251+ramy-att@users.noreply.github.com>
2023-02-21 23:12:40 -0500

    Fix security breach

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
-const mongoose = require("mongoose");
-const cors = require("cors");
-const express = require("express");
+const mongoose = require("mongoose"); //Database
+const cors = require("cors"); //HTTP Connection
+const express = require("express"); //Express
+const dotenv = require("dotenv"); //.env
+const path = require("path"); //to get path to .env
 
 const studentRouter = require("./routes/student-routes");
 
@@ -9,14 +11,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/students", studentRouter); // connected to the local host
+dotenv.config({ path: path.resolve(__dirname, "./.env") });
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
 mongoose.set("strictQuery", false);
 mongoose
   .connect(
-    "mongodb+srv://admin:MkfbFlvNy2W1FwKh@cluster0.gdygsmu.mongodb.net/?retryWrites=true&w=majority"
+    `mongodb+srv://admin:${process.env.DB_KEY}@cluster0.gdygsmu.mongodb.net/?retryWrites=true&w=majority`
   )
   .then(() =>
     app.listen(PORT, () => {
