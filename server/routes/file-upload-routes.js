f5812ee454e48c2639c11f24a94db7e71395d5e8
TarekElalfi <elalfi.tarek@hotmail.com>
2023-03-01 19:46:11 -0500

    #55 resume progress 7:46pm 1st march

--- server/routes/file-upload-routes.js
+++ server/routes/file-upload-routes.js
@@ -2,14 +2,12 @@
 
 const express = require('express');
 const {upload} = require('../helpers/filehelper');
-const {singleFileUpload} = require('../controller/fileuploaderController');
+const {singleFileUpload,getallSingleFiles} = require('../controller/fileuploaderController');
 const router = express.Router();
 
 
 router.post('/singleFile', upload.single('file'), singleFileUpload);
-// router.post('/multipleFiles', upload.array('files'), multipleFileUpload);
-// router.get('/getSingleFiles', getallSingleFiles);
-// router.get('/getMultipleFiles', getallMultipleFiles);
+router.get('/getSingleFiles', getallSingleFiles);
 
 
 module.exports = {
