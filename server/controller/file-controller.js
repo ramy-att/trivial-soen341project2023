2c228209b31ed2c6111e135f2931b0f20692ddcb
MazennW <mazen@almaram.com>
2023-03-10 23:56:58 -0500

    file controller for all file functions

--- server/controller/file-controller.js
+++ server/controller/file-controller.js
@@ -1,5 +1,6 @@
-const downloadFile = async (req, res, next) => {
-    const file = `${__dirname}/server/uploads/` + req.id ;
+const downloadFile = async (req, res) => {
+    const file = `${__dirname}/uploads/` + req.studentId + '.pdf' ;
     res.download(file); // Set disposition and send it.
 }
 
+exports.downloadFile=downloadFile;
