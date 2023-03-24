73d780dbe5e449d1b6559af927ab71a4d5e9d34c
Ramy Attalla <83047251+ramy-att@users.noreply.github.com>
2023-03-24 18:45:38 -0400

    rooutes

--- client/src/App.js
+++ client/src/App.js
@@ -43,42 +43,37 @@ const App = () => {
     } catch (error) {}
   };
   const redirect = () => {
+    const studentRoutes = [
+      "/edit-profile-student",
+      "/signup-stu",
+      "/job-applications",
+    ];
+    const employerRoutes = ["/signup-emp", "/edit-profile-employer"];
+    const authRoutes = ["/SignIn"];
     verifyUser().then((result) => {
       const user = result.user;
       setUserInfo(user ? { ...user } : "nonAuth");
-      if (user && user.type === "student") {
-        if (
-          window.location.pathname === "/signup-emp" ||
-          window.location.pathname === "/edit-profile-employer"
-        ) {
-          history.push("/job-postings");
-        }
-      } else if (user && user.type === "employer") {
-        if (
-          window.location.pathname === "/signup-stu" ||
-          window.location.pathname === "/edit-profile-student"
-        ) {
-          history.push("/job-postings");
-        }
-      }
       if (
-        window.location.pathname === "/SignIn" ||
-        window.location.pathname === "/signup-emp" ||
-        (window.location.pathname === "/signup-student" && user)
+        (user &&
+          user.type === "student" &&
+          !studentRoutes.includes(window.location.pathname)) ||
+        (user &&
+          user.type === "employer" &&
+          !employerRoutes.includes(window.location.pathname))
       ) {
         history.push("/job-postings");
+      } else if (window.location.pathnam === "SignIn" && user) {
+        history.push("/job-postings");
+      } else {
+        history.push("/SignIn");
       }
     });
   };
-  useEffect(
-    () => {
-      redirect();
-      console.log(window.location.pathname);
-    },
-    [
-      /*window.location.pathname, userInfo*/
-    ]
-  );
+  useEffect(() => {
+    redirect();
+    console.log(window.location.pathname);
+  }, []);
+
   return (
     <UserContext.Provider value={userInfo}>
       <div className="App">
@@ -102,7 +97,9 @@ const App = () => {
             <ApplicationsPage userInfo={userInfo} />
           </Route>
           <Route exact path="/job-postings/:id">
+            <OurNav />
             <Posting userInfo={userInfo} />
+            <Footer />
           </Route>
           <Route exact path="/edit-profile-student">
             <Editprofile />
@@ -120,3 +117,26 @@ const App = () => {
 };
 
 export default App;
+
+// if (user && user.type === "student") {
+//   if (
+//     window.location.pathname === "/signup-emp" ||
+//     window.location.pathname === "/edit-profile-employer"
+//   ) {
+//     history.push("/job-postings");
+//   }
+// } else if (user && user.type === "employer") {
+//   if (
+//     window.location.pathname === "/signup-stu" ||
+//     window.location.pathname === "/edit-profile-student"
+//   ) {
+//     history.push("/job-postings");
+//   }
+// }
+// if (
+//   window.location.pathname === "/SignIn" ||
+//   window.location.pathname === "/signup-emp" ||
+//   (window.location.pathname === "/signup-student" && user)
+// ) {
+//   history.push("/job-postings");
+// }
