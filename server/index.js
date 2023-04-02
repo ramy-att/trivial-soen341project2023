fc53471baede06999568b835a3579e62d0731bff
Ramy Attalla <83047251+ramy-att@users.noreply.github.com>
2023-04-01 23:02:48 -0400

    create in memory database and employer tests

--- server/index.js
+++ server/index.js
@@ -55,14 +55,17 @@ app.post("/", (req, res) => {
 
 dotenv.config({ path: path.resolve(__dirname, "./.env") });
 
-mongoose.set("strictQuery", false);
-mongoose
-  .connect(
-    `mongodb+srv://admin:${process.env.DB_KEY}@cluster0.gdygsmu.mongodb.net/?retryWrites=true&w=majority`
-  )
-  .then(() =>
-    app.listen(PORT, () => {
-      console.log(`Server listening on ${PORT}`);
-    })
-  )
-  .catch((err) => console.log("Could not connect to the database"));
+if (process.env.NODE_ENV !== "test") {
+  mongoose.set("strictQuery", false);
+  mongoose
+    .connect(
+      `mongodb+srv://admin:${process.env.DB_KEY}@cluster0.gdygsmu.mongodb.net/?retryWrites=true&w=majority`
+    )
+    .then(() =>
+      app.listen(PORT, () => {
+        console.log(`Server listening on ${PORT}`);
+      })
+    )
+    .catch((err) => console.log("Could not connect to the database"));
+}
+module.exports = app;
