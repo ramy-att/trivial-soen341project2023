4e8023fe23127b8a8a8faecd48692d23efc3b023
Ramy Attalla <83047251+ramy-att@users.noreply.github.com>
2023-04-07 09:43:24 -0400

    fixed failing test

--- tests/Posting.test.js
+++ tests/Posting.test.js
@@ -7,76 +7,81 @@ const posting = require("../server/model/posting");
 let mongoServer;
 
 const employersData = [
-    {
-      employerName: "Employer 1",
-      employerPassword: "employerpass",
-      employerEmail: "emp1@emp1.com",
-      organizationName: "emp1 company",
-    },
-    {
-      employerName: "Employer 2",
-      employerPassword: "employerpass",
-      employerEmail: "emp2@emp2.com",
-      organizationName: "emp2 company",
-    },
-    {
-      employerName: "Employer 3",
-      employerPassword: "employerpassword",
-      employerEmail: "emp3@emp3.com",
-      organizationName: "emp3 company",
-    },
-    {
-      employerName: "Employer 4",
-      employerPassword: "employerpassword",
-      employerEmail: "emp4@emp4.com",
-      organizationName: "emp4 company",
-    },
-  ];
-
+  {
+    employerName: "Employer 1",
+    employerPassword: "employerpass",
+    employerEmail: "emp1@emp1.com",
+    organizationName: "emp1 company",
+  },
+  {
+    employerName: "Employer 2",
+    employerPassword: "employerpass",
+    employerEmail: "emp2@emp2.com",
+    organizationName: "emp2 company",
+  },
+  {
+    employerName: "Employer 3",
+    employerPassword: "employerpassword",
+    employerEmail: "emp3@emp3.com",
+    organizationName: "emp3 company",
+  },
+  {
+    employerName: "Employer 4",
+    employerPassword: "employerpassword",
+    employerEmail: "emp4@emp4.com",
+    organizationName: "emp4 company",
+  },
+];
 
 const postingsData = [
   {
     employerID: "Employer1",
+    postingId: "Posting 1",
     description: "Posting1Employer1",
     title: "EMP1INTERN",
-    expirationDate:"2020",
-    location:"Remote",
+    expirationDate: "2020",
+    location: "Remote",
   },
   {
     employerID: "Employer2",
+    postingId: "Posting 2",
     organizationName: "Employer2Company",
     description: "Posting1 Employer2",
     title: "EMP2 INTERN",
-    expirationDate:"2021",
-    location:"HYBRID",
+    expirationDate: "2021",
+    location: "HYBRID",
   },
   {
     employerID: "Employer1",
+    postingId: "Posting 3",
     description: "Posting2 Employer1",
     title: "EMP1 INTERN2",
-    expirationDate:"2022",
-    location:"Remote2",
+    expirationDate: "2022",
+    location: "Remote2",
   },
   {
     employerID: "Employer2",
+    postingId: "Posting 4",
     description: "Posting2 Employer2",
     title: "EMP2 INTERN2",
-    expirationDate:"2023",
-    location:"HYBRID",
+    expirationDate: "2023",
+    location: "HYBRID",
   },
   {
     employerID: "Employer1",
+    postingId: "Posting 5",
     description: "Posting3 Employer1",
     title: "EMP1 INTERN3",
-    expirationDate:"2024",
-    location:"Remote3",
+    expirationDate: "2024",
+    location: "Remote3",
   },
   {
     employerID: "Employer3",
+    postingId: "Posting 6",
     description: "Posting1 Employer3",
     title: "EMP3 INTERN",
-    expirationDate:"2025",
-    location:"ONSITEEE",
+    expirationDate: "2025",
+    location: "ONSITEEE",
   },
 ];
 beforeAll(async () => {
@@ -97,22 +102,22 @@ afterAll(async () => {
 
 describe("Posting tests", () => {
   it("Create posting", async () => {
-
     // add employer and set posting's employerID.
     let validEmployer = employersData[0];
-    const employerResponse = await request(app).post("/employers").send(validEmployer);
-    const { employer } = employerResponse.body; 
-    //console.log(employer);
-    postingsData[0].employerID=employer._id;
-
+    const employerResponse = await request(app)
+      .post("/employers")
+      .send(validEmployer);
+    const { employer } = employerResponse.body;
+    postingsData[0].employerID = employer._id; // set postingsData[0] employer ID to the correct value
     expect(employerResponse.status).toBe(201); //extra check for employer
 
     let validPosting = postingsData[0];
 
-    //console.log(validPosting);
-
     const response = await request(app).post("/postings").send(validPosting);
     const { posting } = response.body; // get the posting object from the response
+
+    postingsData[0].postingID = posting._id;
+
     expect(response.status).toBe(201);
     expect(posting._id).toBeDefined();
     expect(posting.employerID).toBe(validPosting.employerID);
@@ -122,69 +127,58 @@ describe("Posting tests", () => {
     expect(posting.expirationDate).toBe(validPosting.expirationDate);
     expect(posting.location).toBe(validPosting.location);
   });
-  
+
   it("Delete posting", async () => {
+    // Delete Posting added above
+    const employerID = postingsData[0].employerID;
+    const postingID = postingsData[0].postingID;
 
-    // Create employer to make a posting
-    let validEmployer = employersData[1];
-    const employerResponse = await request(app).post("/employers").send(validEmployer);
-    const { employer } = employerResponse.body; 
+    // delete posting
+    const deleteResponse = await request(app)
+      .delete(`/postings/${postingID}`)
+      .send({ employerID });
 
-    //Create a posting to test
-    postingsData[1].employerID=employer._id;
+    const deletedPosting = await posting.findById(postingID);
 
-    let employerID = employer._id;
-
-    let validPosting = postingsData[1];
-
-    const createResponse = await request(app).post("/postings").send(validPosting);
-    const { newPosting } = createResponse.body; 
-
-    console.log(newPosting);
-
-    //delete posting
-    const response = await request(app).delete(`/postings/${createResponse.body._id}`).send({employerID});
-    expect(response.status).toBe(200);
-
-    const deletedPosting = await posting.findById(newPosting._id);
+    expect(deleteResponse.status).toBe(200);
     expect(deletedPosting).toBeNull();
   });
-//   it("Get all employers", async () => {
-//     const response = await request(app).get(`/employers`);
-//     const { employer } = response.body; // get the employer object from the response
+  //   it("Get all employers", async () => {
+  //     const response = await request(app).get(`/employers`);
+  //     const { employer } = response.body; // get the employer object from the response
 
-//     expect(response.status).toBe(200);
-//     expect(employer[0].employerEmail).toBe("emp1@emp1.com");
-//   });
-//   it("Get employer", async () => {
-//     let validEmployer = employersData[1];
+  //     expect(response.status).toBe(200);
+  //     expect(employer[0].employerEmail).toBe("emp1@emp1.com");
+  //   });
+  //   it("Get employer", async () => {
+  //     let validEmployer = employersData[1];
 
-//     const newEmployer = await employer(validEmployer);
-//     await newEmployer.save();
+  //     const newEmployer = await employer(validEmployer);
+  //     await newEmployer.save();
 
-//     const response = await request(app).get(`/employers/${newEmployer._id}`);
-//     const { employer: employerObj } = response.body; // get the employer object from the response
+  //     const response = await request(app).get(`/employers/${newEmployer._id}`);
+  //     const { employer: employerObj } = response.body; // get the employer object from the response
 
-//     expect(response.status).toBe(200);
-//     expect(employerObj._id).toBeDefined();
-//     expect(employerObj.employerName).toBe(validEmployer.employerName);
-//     expect(employerObj.employerEmail).toBe(validEmployer.employerEmail);
-//     expect(employerObj.organizationName).toBe(validEmployer.organizationName);
-//   });
-//   it("Update employer", async () => {
-//     let validEmployer = employersData[2];
-//     const newEmp = employersData[3];
+  //     expect(response.status).toBe(200);
+  //     expect(employerObj._id).toBeDefined();
+  //     expect(employerObj.employerName).toBe(validEmployer.employerName);
+  //     expect(employerObj.employerEmail).toBe(validEmployer.employerEmail);
+  //     expect(employerObj.organizationName).toBe(validEmployer.organizationName);
+  //   });
+  //   it("Update employer", async () => {
+  //     let validEmployer = employersData[2];
+  //     const newEmp = employersData[3];
 
-//     const newEmployer = await employer(validEmployer);
-//     await newEmployer.save();
+  //     const newEmployer = await employer(validEmployer);
+  //     await newEmployer.save();
 
-//     const response = await request(app)
-//       .patch(`/employers/${newEmployer._id}`)
-//       .send(newEmp);
+  //     const response = await request(app)
+  //       .patch(`/employers/${newEmployer._id}`)
+  //       .send(newEmp);
 
-//     const { message } = response.body;
+  //     const { message } = response.body;
 
-//     expect(response.status).toBe(200);
-//     expect(message).toBe("updated Successfully");
-//   });
+  //     expect(response.status).toBe(200);
+  //     expect(message).toBe("updated Successfully");
+  //   });
 });
