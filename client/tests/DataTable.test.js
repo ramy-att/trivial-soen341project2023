d6c37cb77d4313141239f8e683bfc633c872eb33
Khaled Saleh <93226474+khaled106@users.noreply.github.com>
2023-04-10 17:46:45 -0400

    Added more data and tested all columns.

--- client/tests/DataTable.test.js
+++ client/tests/DataTable.test.js
@@ -5,6 +5,18 @@ import DataTable from "../src/components/DataTable/DataTable";
 
 const sampleData = [
   { id: 0, organization: "Triviality", Position: "CEO", Status: "Accepted" },
+  {
+    id: 1,
+    organization: "Kholio",
+    Position: "BackEnd Devleoper",
+    Status: "Pending",
+  },
+  {
+    id: 2,
+    organization: "Trivial Inc.",
+    Position: "EMP2INTERN",
+    Status: "Rejected",
+  },
 ];
 const headers = ["#", "Organization", "Position", "Status"];
 
@@ -18,6 +30,22 @@ describe("DataTable", () => {
       />
     );
     expect(screen.getByText("Organization")).toBeInTheDocument();
+    expect(screen.getByText("#")).toBeInTheDocument();
+    expect(screen.getByText("Position")).toBeInTheDocument();
+    expect(screen.getByText("Status")).toBeInTheDocument();
+
+    // Check if data is rendered correctly
+    expect(screen.getByText("Triviality")).toBeInTheDocument();
+    expect(screen.getByText("CEO")).toBeInTheDocument();
+    expect(screen.getByText("Accepted")).toBeInTheDocument();
+
+    expect(screen.getByText("Kholio")).toBeInTheDocument();
+    expect(screen.getByText("BackEnd Devleoper")).toBeInTheDocument();
+    expect(screen.getByText("Pending")).toBeInTheDocument();
+
+    expect(screen.getByText("Trivial Inc.")).toBeInTheDocument();
+    expect(screen.getByText("EMP2INTERN")).toBeInTheDocument();
+    expect(screen.getByText("Rejected")).toBeInTheDocument();
   });
 });
 
